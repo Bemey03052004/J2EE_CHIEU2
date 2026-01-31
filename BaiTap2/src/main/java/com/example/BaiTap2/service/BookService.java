bf20b84bb884a5ada5f2346f0c0239c754c926fc
Vo Tuan Anh <votuananh.240604@gmail.com>
2026-01-31 16:05:17 +0700

    update Buoi3

--- BaiTap2/src/main/java/com/example/BaiTap2/service/BookService.java
+++ BaiTap2/src/main/java/com/example/BaiTap2/service/BookService.java
@@ -1,39 +1,43 @@
 package com.example.BaiTap2.service;
 
-import jakarta.annotation.PostConstruct;
 import com.example.BaiTap2.model.Book;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
-    private List<Book> books = new ArrayList<>();
-    @PostConstruct
-    public void initBooks() {
-        books.add(new Book(1, "Lập trình Spring boot", "Võ Tuấn Anh"));
-        books.add(new Book(2, "Lập trình hướng đối tượng", "Võ Tuấn Anh"));
 
-    }
+    private List<Book> books = new ArrayList<>();
+    private int nextId = 1;
+
     public List<Book> getAllBooks() {
         return books;
     }
-    public Book getBookById(int id) {
-        return books.stream().filter(b -> b.getId() == id).findFirst().get();
-    }
+
     public void addBook(Book book) {
+        book.setId(nextId++);
         books.add(book);
     }
-    public void updateBook(int id, Book updatedBook) {
-        books.stream()
+
+    public Optional<Book> getBookById(int id) {
+        return books.stream()
                 .filter(book -> book.getId() == id)
+                .findFirst();
+    }
+
+    public void updateBook(Book updatedBook) {
+        books.stream()
+                .filter(book -> book.getId() == updatedBook.getId())
                 .findFirst()
                 .ifPresent(book -> {
                     book.setTitle(updatedBook.getTitle());
                     book.setAuthor(updatedBook.getAuthor());
                 });
     }
+
     public void deleteBook(int id) {
         books.removeIf(book -> book.getId() == id);
     }
